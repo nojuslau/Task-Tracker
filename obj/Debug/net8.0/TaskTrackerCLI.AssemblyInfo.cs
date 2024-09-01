d076be5b1c7d29d1dbf8c1cd36b291b5bb69fe16
AFG <nojus.lau@gmail.com>
2024-09-01 18:08:04 +0300

    readme

--- obj/Debug/net8.0/TaskTrackerCLI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskTrackerCLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTrackerCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db210320befe6c3d39a59dcabb76df030fc919f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+998dac28042af0f0c5d19112ea0606f0b6665ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTrackerCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTrackerCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
